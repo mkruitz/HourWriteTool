f7dbfc928f4233cf3299a6a33b5fc82f3bad42b8
Martin Kruitz <martin.kruitz@hightechict.nl>
2014-12-12 23:14:23 +0100

    Created base structure

---
+++ Core/HourWriteEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Core
+{
+    public class HourWriteEvent
+    {
+        public DateTime HappendOn { get; set; }
+        public HourWriteType Type { get; set; }
+        public String Remark { get; set; }
+    }
+
+    public enum HourWriteType
+    {
+        StartWork,
+        StartPauze,
+        StopPauze,
+        StopWork
+    }
+}
