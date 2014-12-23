ba7c5a8a82c23c20551f5ac64d18b18670a9fd08
Martin Kruitz <martin.kruitz@hightechict.nl>
2014-12-23 21:03:43 +0100

    Adding new Edit UserControl

---
+++ GUI/UserControlEditRecords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Core;
+
+namespace GUI
+{
+    public partial class UserControlEditRecords : UserControlWithStore
+    {
+        public UserControlEditRecords()
+        {
+            InitializeComponent();
+            StoreChanged += OnStoreChanged;
+            DefaultSize = new Size(388, 273);
+        }
+
+        private void OnStoreChanged()
+        {
+            dataGridView.DataSource = Store.GetEvents();
+        }
+    }
+}
